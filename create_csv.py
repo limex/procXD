54d81628857d05604964b0d3dc43b794f67c322f
limex <guebosch@gmail.com>
2024-02-23 15:37:03 +0100

    added misc. layouts

--- create_csv.py
+++ create_csv.py
@@ -2,7 +2,7 @@ import csv
 import random
 
 # Generate 20 pairs of random integers between 1 and 40
-pairs = [(random.randint(1, 10), random.randint(1, 10)) for _ in range(40)]
+pairs = [(random.randint(1, 10), random.randint(1, 10)) for _ in range(25)]
 
 # Write the pairs to a CSV file
 with open('data.csv', 'w', newline='') as file:
